02a593abfc019788b742c77add011679e7cbf1fe
Drew Croteau <requin@csu.fullerton.edu>
2020-12-09 17:50:40 -0800

    Integrated proper Employee and Customer functionality

--- app/src/main/java/com/example/adaptiveservice/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/adaptiveservice/ui/login/LoginActivity.kt
@@ -101,7 +101,7 @@ class LoginActivity : AppCompatActivity() {
 
     private fun updateUiWithUser(model: LoggedInUserView) {
         val welcome = getString(R.string.welcome)
-        val displayName = model.displayName
+        val displayName = username.text.toString()//model.displayName
         // TODO : initiate successful logged in experience
         Toast.makeText(
                 applicationContext,
